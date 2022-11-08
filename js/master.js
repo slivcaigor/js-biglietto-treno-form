a331c128c6122c3f92cf4a18ca8dae2c221987e4
slivcaigor <slivcaigor@gmail.com>
2022-11-08 15:24:40 +0100

    Structure

---
+++ js/master.js
@@ -0,0 +1,26 @@
+// Chiedere il nome e cognome del passeggero all'interno del input
+
+// Chiedere la distanza in KM da percorrere all'interno del input
+
+// Chiedere l'età del passeggero all'interno del input
+
+// Dare l'opzione di annullare con un secondo tasto
+
+// Al click del tasto "Genera"
+
+  // Avvisare di uno sconto del 20% tramite popup, se minorenne
+
+  // Avvisare di uno sconto del 40% tramite popup, se over 65
+
+  // Mostrare nome passeggero 
+
+  // Generare un numero random per Carrozza
+
+  // Generare un numero random per Codice CP
+
+  // Mostrare costo biglietto con sconto applicato
+
+
+
+
+
